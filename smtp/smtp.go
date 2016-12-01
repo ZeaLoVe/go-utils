2be9b7c5764b135ad3837e63ab1fc011967a2845
ZeaLoVe <ruanguiliang_34@whu.edu.cn>
2016-12-01 17:07:48 +0800

    modify smtp sendmail

--- smtp/smtp.go
+++ smtp/smtp.go
@@ -1,7 +1,9 @@
 package smtp
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net"
 	"net/smtp"
 	"strings"
 	"sync"
@@ -46,6 +48,62 @@ func (this *SmtpMailSender) SetMailAcount(acount *MailAcount) error {
 	return nil
 }
 
+func CopyValideInfo(list []string) []string {
+	var ret []string
+	for _, val := range list {
+		if val != "" {
+			ret = append(ret, val)
+		}
+	}
+	return ret
+}
+
+/*修改原来的sendmail函数*/
+func MySendMail(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		host, _, _ := net.SplitHostPort(addr)
+		config := &tls.Config{InsecureSkipVerify: true, ServerName: host}
+		if err = c.StartTLS(config); err != nil {
+			return err
+		}
+	}
+	if a != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(a); err != nil {
+				return err
+			}
+		}
+	}
+
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
+
 func (this *SmtpMailSender) SendMail(mail *Mail) error {
 	this.lock.Lock()
 	defer this.lock.Unlock()
@@ -55,7 +113,7 @@ func (this *SmtpMailSender) SendMail(mail *Mail) error {
 	if mail.Tos == "" || mail.Content == "" || mail.Subject == "" {
 		return fmt.Errorf("mail infomation lack")
 	}
-	err := smtp.SendMail(this.ac.Server+SMTPPORTSTRING, this.auth, this.ac.User, strings.Split(mail.Tos, ","), []byte(mail.String()))
+	err := MySendMail(this.ac.Server+SMTPPORTSTRING, this.auth, this.ac.User, CopyValideInfo(strings.Split(mail.Tos, ",")), []byte(mail.String()))
 	if err != nil {
 		return err
 	}
